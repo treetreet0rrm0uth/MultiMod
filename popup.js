f12191b42a1240814fd21c27c0b08272b2f68f52
treetreet0rrm0uth <80350771+treetreet0rrm0uth@users.noreply.github.com>
2022-01-29 11:25:39 -0800

    commit

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
 window.onload = function () {
     document.getElementById("insertIframe").addEventListener("click", insertIframe);
-    document.getElementById("executeJavaScript").addEventListener("click", insertIframe);
+    document.getElementById("executeJavaScript").addEventListener("click", executeJavaScript);
 }
 function insertIframe() {
-    chrome.tabs.executeScript({file: "insertIframe.js"});
-    chrome.tabs.executeScript({file: "executeJavaScript.js"})
+    chrome.tabs.executeScript({file: "scripts/insertIframe.js"});
+}
+function executeJavaScript() {
+    chrome.tabs.executeScript({file: "scripts/executeJavaScript.js"});
 }
